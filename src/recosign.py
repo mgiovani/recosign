d69b9b8636ecc755f79429faf8840c56ca355b0f
Camila Lopes <kmilalopes10@gmail.com>
2019-06-21 10:05:13 -0300

    Redimensiona imagem
    - Recorta primeira assinatura

--- src/recosign.py
+++ src/recosign.py
@@ -1,4 +1,15 @@
 import cv2 as cv
-imagem = cv.imread('../data/assinaturas.png')
-cv.imshow("Folha de assinaturas", imagem)
+
+imagem = cv.imread('../data/assinaturas.jpg')
+
+# Redimensionamento
+proporcao = 700.0 / imagem.shape[1]
+tamanho_novo = (700, int(imagem.shape[0] * proporcao))
+imagem_redimensionada = cv.resize(imagem, tamanho_novo, interpolation = cv.INTER_AREA)
+cv.imshow("Imagem redimensionada", imagem_redimensionada)
+
+# Recorte
+recorte = imagem_redimensionada[74:160, 74:625]
+cv.imshow("Recorte da imagem", recorte)
+
 cv.waitKey(0)
