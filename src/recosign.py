c4268a7d8d36e985110f249d4487707d77826512
Camila Lopes <kmilalopes10@gmail.com>
2019-06-26 11:13:34 -0300

    Adiciona binarização da imagem
    - Calcula a media das linhas da matriz de caracteristicas

--- src/recosign.py
+++ src/recosign.py
@@ -1,4 +1,10 @@
+# shape 0 linha
+# shape 1 coluna
 import cv2 as cv
+import numpy as np
+
+# Visualizar a matriz completa no print
+np.set_printoptions(threshold=np.inf)
 
 imagem = cv.imread('../data/assinaturas.jpg', 0)
 
@@ -15,13 +21,25 @@ cv.imshow("Imagem redimensionada", imagem_redimensionada)
 # Recorte
 assinaturas = []
 altura_retangulo = 85
-assinaturas.append(imagem_redimensionada[74:160, 74:625])
+assinaturas.append(imagem_redimensionada[76:158, 76:623])
 for i in range(160, 925, altura_retangulo):
-    assinaturas.append(imagem_redimensionada[i:i+altura_retangulo, 74:625])
+    assinaturas.append(imagem_redimensionada[i:i+altura_retangulo, 76:615])
 
 for assinatura in assinaturas:
-    cv.imshow("Assinatura", assinatura)
+    # Binarização da assinatura
+    for i in range(assinatura.shape[0]):
+        for j in range(assinatura.shape[1]):
+            if assinatura[i][j] < 220:
+                assinatura[i][j] = 0
+            else:
+                assinatura[i][j] = 255
+
     # Extração de cantos da assinatura
     quinas = cv.cornerHarris(assinatura,2,3,0.04)
-    print("quinas: ", quinas)
+    # Caracteristicas
+    for i in range(quinas.shape[0]):
+        media = np.sum(quinas[i])/quinas.shape[0]
+
+    print(quinas)
+    cv.imshow("Imagem quina", quinas)
     cv.waitKey(0)
