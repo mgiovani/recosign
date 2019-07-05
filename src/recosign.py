fd00437fac57706530e05bc6dd5ea716747cd6a7
Camila Lopes <kmilalopes10@gmail.com>
2019-07-05 10:10:51 -0300

    Adiciona extração de caracteristicas
    - Fixa o tamanho do vetor de caracteristicas

--- src/recosign.py
+++ src/recosign.py
@@ -16,7 +16,7 @@ imagem = cv.GaussianBlur(imagem, (9, 9), 0)
 proporcao = 700.0 / imagem.shape[1]
 tamanho_novo = (700, int(imagem.shape[0] * proporcao))
 imagem_redimensionada = cv.resize(imagem, tamanho_novo, interpolation = cv.INTER_AREA)
-cv.imshow("Imagem redimensionada", imagem_redimensionada)
+# cv.imshow("Imagem redimensionada", imagem_redimensionada)
 
 # Recorte
 assinaturas = []
@@ -26,7 +26,7 @@ for i in range(160, 925, altura_retangulo):
     assinaturas.append(imagem_redimensionada[i+5:i+altura_retangulo-5, 76:615])
 
 for assinatura in assinaturas:
-    cv.imshow("Assinatura cortada", assinatura)
+    # cv.imshow("Assinatura cortada", assinatura)
 
     # Binarização da assinatura
     for i in range(assinatura.shape[0]):
@@ -35,6 +35,8 @@ for assinatura in assinaturas:
                 assinatura[i][j] = 0
             else:
                 assinatura[i][j] = 255
+    
+    #Fit(recorte) da assinatura nas laterais
     x1 = None
     for j in range(assinatura.shape[1]):
         for i in range(assinatura.shape[0]):
@@ -53,6 +55,22 @@ for assinatura in assinaturas:
             break
 
     assinatura = assinatura[0:assinatura.shape[0], x1:x2]
+    
+    #Extrai caracteristicas
+    caracteristicas = [0] * assinatura.shape[1]
+    for j in range(assinatura.shape[1]):
+        for i in range(assinatura.shape[0]):
+            if assinatura[i][j] == 0:
+                caracteristicas[j] += 1
+
+    #Trata os vetores de caracteristicas fixando a dimensao em 300
+    if len(caracteristicas) < 300:
+        for _ in range(len(caracteristicas)-1, 300):
+            caracteristicas.append(0)
+    else:
+        caracteristicas = caracteristicas[0:300]
+
+    print(caracteristicas)
 
     cv.imshow("Imagem quina", assinatura)
     cv.waitKey(0)
